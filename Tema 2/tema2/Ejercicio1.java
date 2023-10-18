de9e24e92d2c36cae4843a041074fcfe509a626a
Condicionales <valcarceljuandavid@gmail.com>
2023-10-18 22:52:36 +0200

    Funciones

--- Tema 2/tema2/Ejercicio1.java
+++ Tema 2/tema2/Ejercicio1.java
@@ -1,4 +1,15 @@
 package tema2;
 
 public class Ejercicio1 {
+    //public class Foo{//
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] a) {
+        int result = add(2, 3);
+        System.out.println(result);
+    }
 }
+
+
