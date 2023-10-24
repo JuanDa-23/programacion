af83fe32e4fcc23b495651b78c45b1b28c47ff97
juamarval5 <valcarceljuandavid@gmail.com>
2023-10-24 09:44:12 +0200

    Funciones

--- Tema 2/tema2/Ejercicio4.java
+++ Tema 2/tema2/Ejercicio4.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Ejercicio4 {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Introduce el dinero, te lo pasaré de € a $ o viceversa");
         int money = scanner.nextInt();
