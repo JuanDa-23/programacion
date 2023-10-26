e2ca8b55620c65a8a844944c0682adde727a5d61
juamarval5 <valcarceljuandavid@gmail.com>
2023-10-26 10:47:06 +0200

    Correciones  Funciones

--- Tema 2/tema2/Ejercicio6.java
+++ Tema 2/tema2/Ejercicio6.java
@@ -6,21 +6,21 @@ public class Ejercicio6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = 1;
-      //  System.out.println(multiplyNumber(number));
+        showTableMultiplication(number);
     }
 
 
     public static void showTableMultiplication(int number) {
 
         int result = 1;
-        for (number = 1; number <= 10; number++)
+        for (number = 1; number < 10; number++)
             Ejercicio5.showMultiplication(number);
             for (int i = 1; i <= 10; i++) {
                 result = i * number;
                 System.out.println(i + " x " + number + " = " + result);
 
             }
-       // return result;
+
     }
 
 }
--- Tema 2/tema2/Ejercicio9.java
+++ Tema 2/tema2/Ejercicio9.java
@@ -9,15 +9,15 @@ public class Ejercicio9 {
     }
 
     public static void showTriangle(char character, int numberLine) {
-       /* for (int i = 1; i <= numberLine; i++) {
+        /*for (int i = 1; i <= numberLine; i++) {
             for (int j = 1; j <= numberLine - i; j++) {
                 System.out.print(" ");
             }
             for (int k = 1; k <= 2 * i - 1; k++) {
                 System.out.print(character);
             }
-            System.out.println();
-        }*/
+            System.out.println();*/
+        }
     }
-}
+
 
