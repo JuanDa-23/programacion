130eb3883539b85907d5c05bcb83c132a978464a
Condicionales <valcarceljuandavid@gmail.com>
2023-10-27 18:16:39 +0200

    Correción

--- Tema 2/tema2/Ejercicio9.java
+++ Tema 2/tema2/Ejercicio9.java
@@ -1,7 +1,5 @@
 package tema2;
 
-import java.util.Scanner;
-
 public class Ejercicio9 {
     public static void main(String[] args) {
 
@@ -9,15 +7,15 @@ public class Ejercicio9 {
     }
 
     public static void showTriangle(char character, int numberLine) {
-        /*for (int i = 1; i <= numberLine; i++) {
+        for (int i = 1; i <= numberLine; i++) {
             for (int j = 1; j <= numberLine - i; j++) {
                 System.out.print(" ");
             }
             for (int k = 1; k <= 2 * i - 1; k++) {
                 System.out.print(character);
             }
-            System.out.println();*/
+            System.out.println();
         }
     }
 
-
+}
