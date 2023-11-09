a91b90597869ca2baeaf9a7970072dd888e98276
juamarval5 <valcarceljuandavid@gmail.com>
2023-11-09 10:03:30 +0100

    Utils

--- test/Utils.java
+++ test/Utils.java
@@ -21,22 +21,40 @@ public class Utils {
         return result;
     }
 
-    public static double getCoins(double dinero) {
+    public static double getCoins(double total) {
         int monedas = 0;
+        int dinero = (int) Math.round(total * 100);
         while (dinero > 0) {
-            if (dinero >= 2) {
-            dinero -= 2;
-            monedas += 10000000;
-            } else if (dinero >= 1) {
-            } else if (dinero >= 0.50) {
-            } else if (dinero >= 0.20) {
-            } else if (dinero >= 0.10) {
-            } else if (dinero >= 0.05) {
-            } else if (dinero >= 0.02) {
-            } else if (dinero >= 0.01) {
-
+            if (dinero >= 200) {
+                dinero -= 200;
+                monedas += 10000000;
+            } else if (dinero >= 100) {
+                dinero -= 100;
+                monedas += 1000000;
+            } else if (dinero >= 50) {
+                dinero -= 50;
+                monedas += 100000;
+            } else if (dinero >= 20) {
+                dinero -= 20;
+                monedas += 10000;
+            } else if (dinero >= 10) {
+                dinero -= 10;
+                monedas += 1000;
+            } else if (dinero >= 5) {
+                dinero -= 5;
+                monedas += 100;
+            } else if (dinero >= 2) {
+                dinero -= 2;
+                monedas += 10;
+            } else {
+                dinero -= 1;
+                monedas += 1;
             }
         }
+        return monedas;
+    }
+    public static double getFIF(double letter) {
+
     }
 }
 
