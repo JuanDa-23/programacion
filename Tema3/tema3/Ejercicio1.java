48014a8bdec81910fae2f83efb0c57f639f8ee58
juamarval5 <valcarceljuandavid@gmail.com>
2023-11-22 13:56:18 +0100

    Arrays

--- Tema3/tema3/Ejercicio1.java
+++ Tema3/tema3/Ejercicio1.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        int[] array = new int[]{1,2,3,4,5};
+        array [0] = 0;
+        for (int i = 0; i < array.length; i++) {
+                array[i] = i;
+            System.out.println(array[i]);
+        }
     }
 }
