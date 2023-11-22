4a6787e4454e10e3779092283e41b498b214e3a1
Condicionales <valcarceljuandavid@gmail.com>
2023-11-22 21:07:03 +0100

    Array

--- Tema3/tema3/Ejercicio1.java
+++ Tema3/tema3/Ejercicio1.java
@@ -3,12 +3,21 @@ package tema3;
 import java.util.Scanner;
 
 public class Ejercicio1 {
+    static int[] array = new int[]{1,2,3,4,5};
     public static void main(String[] args) {
-        int[] array = new int[]{1,2,3,4,5};
+
         array [0] = 0;
         for (int i = 0; i < array.length; i++) {
                 array[i] = i;
             System.out.println(array[i]);
         }
     }
+   /* public static int mayorArray(int mayor){
+
+        for (int i = 0; i < array.length; i++) {
+            array[i]= i;
+
+        }
+
+    }*/
 }
