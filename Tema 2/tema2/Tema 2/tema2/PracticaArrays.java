a48548c19e18ca56af8ff0b889c45cca825a96b5
juamarval5 <valcarceljuandavid@gmail.com>
2023-11-22 13:40:15 +0100

    Arrays

--- Tema 2/tema2/Tema 2/tema2/PracticaArrays.java
+++
@@ -1,9 +0,0 @@
-package tema2.Tema;
-
-public class PracticaArrays {
-    public static void main(String[] args) {
-        int[] grades ;
-        grades = new int[] {0, 2, 4, 6, 8, 10, 12, 14, 16, 18};
-        System.out.println(grades[1]);
-    }
-}
