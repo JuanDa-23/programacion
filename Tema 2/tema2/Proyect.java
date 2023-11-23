634269feff6725b976917d0fe43411e2f6acf4d4
juamarval5 <valcarceljuandavid@gmail.com>
2023-11-23 10:41:23 +0100

    The game

--- Tema 2/tema2/Proyect.java
+++ Tema 2/tema2/Proyect.java
@@ -4,8 +4,33 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Proyect {
+    static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
+    static int speed1;
+    static int damage1;
+    static int defense1;
+    static int life1;
+    static int speed2;
+    static int damage2;
+    static int defense2;
+    static int life2;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
+        showMenu();
+        /*if (configureAttributes()) {
+            System.out.println("_______".repeat(10));
+        } else {
+            System.out.println("Vuelve a introducir los valores: ");
+            showMenu();
+        }*/
+        generateRounds();
+
+    }
+
+
+
+    public static void showMenu() {
         System.out.println(" #####  #  #   ####           ##      #    #   #  ####\n" +
                 "   #    #  #   #             #  #    # #   ## ##  #\n" +
                 "   #    ####   ###           #      #   #  # # #  ###\n" +
@@ -14,108 +39,103 @@ public class Proyect {
                 "   #    #  #   ####           ##    #   #  #   #  ####\n");
         System.out.println("Introduce un número del 1 al 200, para asignar valor a los atributos.\nLa suma total de los atributos introducidos no puede superar los 500 puntos");
 
-      //showMenu
-        // ConfigurePoint
-           // doRondas
-
-
         //Introducción Atributos Personaje//
         System.out.print("Introduce los puntos de Velocidad: ");
-        int puntosVelocidad = scanner.nextInt();
+        speed1 = scanner.nextInt();
         System.out.print("Introduce los puntos de Ataque: ");
-        int puntosAtaque = scanner.nextInt();
+        damage1 = scanner.nextInt();
         System.out.print("Introduce los puntos de Defensa: ");
-        int puntosDefensa = scanner.nextInt();
+        defense1 = scanner.nextInt();
         System.out.print("Introduce los puntos de Vida: ");
-        int puntosVida = scanner.nextInt();
-        if ((puntosVelocidad + puntosAtaque + puntosDefensa + puntosVida < 501) && (puntosVelocidad <= 201 && puntosVelocidad > 0 || puntosAtaque <= 201 && puntosAtaque > 0 || puntosDefensa <= 201 && puntosDefensa > 0 || puntosVida <= 201 && puntosVida > 0)) {
-            System.out.println("____________________________________________________________________________________________________");
-        } else {
-            System.out.println("Error, vuelve a introducir los puntos ");
-        }
+        life1 = scanner.nextInt();
 
         //Atributos NPC//
-        Random random = new Random();
-        int randomInt = random.nextInt(50, 200);
         System.out.println("Ahora, le daré yo puntos a mi Guerrero");
+        System.out.println("La Velocidad será de: " + speed2);
+        speed2 = random.nextInt(50, 201);
+        System.out.println("El Ataque será de: " + damage2);
+        damage2 = random.nextInt(50, 201);
+        System.out.println("La Defensa será de: " + defense2);
+        defense2 = random.nextInt(50, 201);
+        System.out.println("La Vida será de: " + life2);
+        life2 = random.nextInt(50, 201);
 
-        int velocidadNpc = random.nextInt(50, 201);
-        System.out.println("La Velocidad será de: " + velocidadNpc);
-        int ataqueNpc = random.nextInt(50, 201);
-        System.out.println("El Ataque será de: " + ataqueNpc);
-        int defensaNpc = random.nextInt(50, 201);
-        System.out.println("La Defensa será de: " + defensaNpc);
-        int vidaNpc = random.nextInt(50, 201);
-        System.out.println("La Vida será de: " + vidaNpc);
-        int totalPoints = velocidadNpc + ataqueNpc + defensaNpc + vidaNpc;
-        while (totalPoints == 500) {
-            if (velocidadNpc > 0 || ataqueNpc > 0 || defensaNpc > 0 || vidaNpc > 0) {
+    }
 
-            }
+    //Bucles entre funciones
+   /* public static boolean configureAttributes() {
+        boolean atributosSonCorrectos = false;
+        while (!atributosSonCorrectos) {
+            atributosSonCorrectos = configureAttributes();
         }
+        if ((speed1 + damage1 + defense1 + life1 < 501) && (speed1 <= 201 && speed1 > 0 || damage1 <= 201 && damage1 > 0 || defense1 <= 201 && defense1 > 0 || life1 <= 201 && life1 > 0)) {
+            atributosSonCorrectos = true;
+        }
+        return atributosSonCorrectos;
+    }*/
 
+        public static void generateRounds() {
         //Generación de RONDAS//
+
         int i = 1;
-        while (puntosVida > 0 && vidaNpc > 0) {
-            System.out.println("____________________________________________________________________________________________________");
-            System.out.println("RONDA " + i);
+        while (life1 > 0 && life2 > 0){
+        System.out.println("_______".repeat(10));
+        System.out.println("RONDA "+i);
 
-            if (puntosVelocidad > velocidadNpc) {
-                System.out.println("Empieza tu personaje");
-
-                int critico = random.nextInt(1, 100);
-                if (critico >= 80) {
-                    puntosAtaque = random.nextInt(100, 201);
-                    System.out.println("Inflinges un crítico de " + (puntosAtaque) + " puntos; mi salud disminuye a " + (Math.max(vidaNpc - puntosAtaque, 0)));
-                } else if (critico < 20) {
-                    puntosAtaque = random.nextInt(50, 101);
-                    puntosDefensa = puntosDefensa - 10;
-                    System.out.println("Solo inflinges " + (puntosAtaque) + " y mi salud disminuye a " + (Math.max(vidaNpc - puntosAtaque, 0)));
-                } else {
-                    System.out.println("Inflinges " + (puntosAtaque) + "; mi salud disminuye a " + Math.max(vidaNpc - puntosAtaque, 0));
-                    System.out.println("Mi Guerrero inflinge " + (ataqueNpc) + "; tu salud disminuye a " + Math.max(puntosVida - ataqueNpc, 0));
-                }
-                System.out.println(Math.max(puntosVida -= ataqueNpc, 0));
-                System.out.println(Math.max(vidaNpc -= puntosAtaque, 0));
-                System.out.println("____________________________________________________________________________________________________");
-
-            }
-            if (puntosVida <= 0) {
-                System.out.println("¡¡¡HE GANADO!!!");
-            } else if (vidaNpc <= 0) {
-                System.out.println("¡¡¡HAS GANADO!!!");
-                System.out.println("____________________________________________________________________________________________________");
-
-
-            } else {
-                System.out.println("Empieza mi Guerrero");
-
-                int critico2 = random.nextInt(1, 100);
-                if (critico2 >= 80) {
-                    ataqueNpc = random.nextInt(100, 201);
-                    System.out.println("Mi Guerrero inflinge un crítico de " + (ataqueNpc) + " puntos; tu salud disminuye a " + Math.max(puntosVida - ataqueNpc, 0));
-                } else if (critico2 < 20) {
-                    ataqueNpc = random.nextInt(50, 101);
-                    defensaNpc = defensaNpc - 10;
-                    System.out.println("Solo inflinges " + (ataqueNpc) + " mi salud disminuye a " + (Math.max(puntosVida - ataqueNpc, 0)));
-                } else {
-                    System.out.println("Mi Guerrero inflinge " + (ataqueNpc) + "; tu salud disminuye a " + Math.max(puntosVida - ataqueNpc, 0));
-                    System.out.println("Inflinges " + (puntosAtaque) + "; mi salud disminuye a " + (Math.max(vidaNpc - puntosAtaque, 0)));
-                }
-                System.out.println(Math.max(puntosVida -= ataqueNpc, 0));
-                System.out.println(Math.max(vidaNpc -= puntosAtaque, 0));
-                System.out.println("____________________________________________________________________________________________________");
-
-            }
-            if (puntosVida <= 0) {
-                System.out.println("¡¡¡HE GANADO!!!");
-            } else if (vidaNpc <= 0) {
-                System.out.println("¡¡¡HAS GANADO!!!");
-                System.out.println("____________________________________________________________________________________________________");
-
-
-            }
+        startFirst();
+        startSecond();
+        gameOver();
+        }
             ++i;
+    }
+    public static void startFirst() {
+        if (speed1 > speed2) {
+            System.out.println("Empieza tu personaje");
+
+            int critico = random.nextInt(1, 100);
+            if (critico >= 80) {
+                damage1 = random.nextInt(100, 201);
+                System.out.println("Inflinges un crítico de " + (damage1) + " puntos; mi salud disminuye a " + (Math.max(life2 - damage1, 0)));
+            } else if (critico < 20) {
+                damage1 = random.nextInt(50, 101);
+                defense1 = defense1 - 10;
+                System.out.println("Solo inflinges " + (damage1) + " y mi salud disminuye a " + (Math.max(life2 - damage1, 0)));
+            } else {
+                System.out.println("Inflinges " + (damage1) + "; mi salud disminuye a " + Math.max(life2 - damage1, 0));
+                System.out.println("Mi Guerrero inflinge " + (damage2) + "; tu salud disminuye a " + Math.max(life1 - damage2, 0));
+            }
+            System.out.println(Math.max(life1 -= damage2, 0));
+            System.out.println(Math.max(life2 -= damage1, 0));
+            System.out.println("________".repeat(10));
+        }
+    }
+    public static void startSecond() {
+
+            System.out.println("Empieza mi Guerrero");
+
+            int critico2 = random.nextInt(1, 100);
+            if (critico2 >= 80) {
+                damage2 = random.nextInt(100, 201);
+                System.out.println("Mi Guerrero inflinge un crítico de " + (damage2) + " puntos; tu salud disminuye a " + Math.max(life1 - damage2, 0));
+            } else if (critico2 < 20) {
+                damage2 = random.nextInt(50, 101);
+                defense2 = defense2 - 10;
+                System.out.println("Solo inflinges " + (damage2) + " mi salud disminuye a " + (Math.max(life1 - damage2, 0)));
+            } else {
+                System.out.println("Mi Guerrero inflinge " + (damage2) + "; tu salud disminuye a " + Math.max(life1 - damage2, 0));
+                System.out.println("Inflinges " + (damage1) + "; mi salud disminuye a " + (Math.max(life2 - damage1, 0)));
+            }
+            System.out.println(Math.max(life1 -= damage2, 0));
+            System.out.println(Math.max(life2 -= damage1, 0));
+            System.out.println("________".repeat(10));
+
+    }
+    public static void gameOver() {
+        if (life1 <= 0) {
+            System.out.println("¡¡¡HE GANADO!!!");
+        } else if (life2 <= 0) {
+            System.out.println("¡¡¡HAS GANADO!!!");
+            System.out.println("_______".repeat(10));
         }
     }
 }
