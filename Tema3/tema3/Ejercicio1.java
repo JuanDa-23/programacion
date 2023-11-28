2aa60c78c412580ff5a1f136aae9b27e0bd94026
juamarval5 <valcarceljuandavid@gmail.com>
2023-11-28 09:56:25 +0100

    Arrays

--- Tema3/tema3/Ejercicio1.java
+++ Tema3/tema3/Ejercicio1.java
@@ -3,21 +3,29 @@ package tema3;
 import java.util.Scanner;
 
 public class Ejercicio1 {
-    static int[] array = new int[]{1,2,3,4,5};
+   /* static int[] array = new int[]{0,1,2,3,4,5};
     public static void main(String[] args) {
+        creatArray();
+        System.out.println(array.toString(max(array)));
+        mayorArray();
 
+    }
+
+    public static void creatArray() {
         array [0] = 0;
         for (int i = 0; i < array.length; i++) {
-                array[i] = i;
+            array[i] = i;
             System.out.println(array[i]);
         }
     }
-   /* public static int mayorArray(int mayor){
-
+   public static int [] mayorArray(int[] array){
+       int max = array[0];
         for (int i = 0; i < array.length; i++) {
-            array[i]= i;
+          if (array[i] > max) {
+              max = array[i];
+          }
 
         }
-
-    }*/
+       return new int[]{max} ;
+   }*/
 }
