5fa15608b0f0344d06a70a3f2e90d61d07b81c25
juamarval5 <valcarceljuandavid@gmail.com>
2023-11-28 10:45:35 +0100

    Arrays

--- Tema3/tema3/Ejercicio1.java
+++ Tema3/tema3/Ejercicio1.java
@@ -3,13 +3,14 @@ package tema3;
 import java.util.Scanner;
 
 public class Ejercicio1 {
-   /* static int[] array = new int[]{0,1,2,3,4,5};
-    public static void main(String[] args) {
+    static int[] array = new int[]{0,1,2,3,4,5};
+    static Scanner scanner = new Scanner(System.in);
+   /* public static void main(String[] args) {
         showArray();
         System.out.println(array.toString(max(array)));
         mayorArray();
 
-    }
+    }*/
 
     public static void showArray() {
         array [0] = 0;
@@ -18,7 +19,7 @@ public class Ejercicio1 {
             System.out.println(array[i]);
         }
     }
-   public static int [] mayorArray(int[] array){
+   public static int [] maxArray(int[] array){
        int max = array[0];
         for (int i = 0; i < array.length; i++) {
           if (array[i] > max) {
@@ -27,5 +28,38 @@ public class Ejercicio1 {
 
         }
        return new int[]{max} ;
-   }*/
+   }
+   public static int [] minArray(int[] array){
+       int min = array[0];
+       for (int i = 0; i < array.length; i++) {
+           if (array[i] < min) {
+               min = array[i];
+           }
+
+       }
+       return new int[]{min} ;
+   }
+   public static int[] mediaArray(int[] array){
+       int sumatory = array[0];
+       for (int i = 0; i < array.length; i++) {
+           sumatory += array[i];
+           }
+       return new int[]{sumatory};
+   }
+
+    public static boolean[] existeArray(int[] array) {
+
+        int number = scanner.nextInt();
+        array [0] = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+            if (number == i) {
+                System.out.println("El número existe.");
+            }else {
+                System.out.println("El número no existe.");
+            }
+        }
+        return new boolean[0];
+        //?
+    }
 }
