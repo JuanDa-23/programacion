0527fa3335ad8b69023683f2b40e4659104b5116
juamarval5 <valcarceljuandavid@gmail.com>
2023-11-29 13:27:44 +0100

    Arrays

--- Tema3/tema3/Ejercicio1.java
+++ Tema3/tema3/Ejercicio1.java
@@ -3,8 +3,10 @@ package tema3;
 import java.util.Scanner;
 
 public class Ejercicio1 {
-    static int[] array = new int[]{0,1,2,3,4,5};
+    static int[] array = new int[]{0, 1, 2, 3, 4, 5};
+    static  int[] array2 = new int[]{7, 5, 9, 0, 8, 3};
     static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         showArray();
         System.out.println("_______".repeat(20));
@@ -19,55 +21,67 @@ public class Ejercicio1 {
     }
 
     public static void showArray() {
-        array [0] = 0;
+        array[0] = 0;
         for (int i = 0; i < array.length; i++) {
             array[i] = i;
             System.out.println(array[i]);
         }
     }
-   public static int maxArray(int[] array){
-       int max = array[0];
+
+    public static int maxArray(int[] array) {
+        int max = array[0];
         for (int i = 0; i < array.length; i++) {
-          if (array[i] > max) {
-              max = array[i];
-          }
+            if (array[i] > max) {
+                max = array[i];
+            }
 
         }
-       return max ;
-   }
-   public static int minArray(int[] array){
-       int min = array[0];
-       for (int i = 0; i < array.length; i++) {
-           if (array[i] < min) {
-               min = array[i];
-           }
+        return max;
+    }
 
-       }
-       return min;
-   }
-   public static int mediaArray(int[] array){
-       int sumatory = array[0];
-       for (int i = 0; i < array.length; i++) {
-           sumatory += array[i] /2;
-           }
-       return sumatory;
-   }
+    public static int minArray(int[] array) {
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+
+        }
+        return min;
+    }
+
+    public static int mediaArray(int[] array) {
+        int sumatory = array[0];
+        for (int i = 0; i < array.length; i++) {
+            sumatory += array[i] / 2;
+        }
+        return sumatory;
+    }
 
     public static void existeArray(int[] array) {
-        array [0] = 0;
+        array[0] = 0;
         for (int i = 0; i < array.length; i++) {
             array[i] = i;
             System.out.println("Introduce un valor:");
             int number = scanner.nextInt();
             if (number == i) {
                 System.out.println("El número existe.");
-            }else {
+            } else {
                 System.out.println("El número no existe.");
-                number++;
+
             }
         }
     }
-    public static int sumatoryArray(int[] array){
+
+    public static int sumatoryArray(int[] array) {
+        int result = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+            for (int j = 0; j < array.length; j++)
+
+            array[i] + array2[j] = result;
+        }
+        return result;
 
     }
 }
