cc580dddba5ffefa4ff863d64442c539638c5fff
juamarval5 <valcarceljuandavid@gmail.com>
2023-11-29 12:59:28 +0100

    Arrays

--- Tema3/tema3/Ejercicio1.java
+++ Tema3/tema3/Ejercicio1.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class Ejercicio1 {
     static int[] array = new int[]{0,1,2,3,4,5};
     static Scanner scanner = new Scanner(System.in);
-   /* public static void main(String[] args) {
+    public static void main(String[] args) {
         showArray();
-        System.out.println(array.toString(max(array)));
-        mayorArray();
+        System.out.println(maxArray(array));
+        System.out.println(minArray(array));
+        System.out.println(mediaArray(array));
+        existeArray(array);
 
-    }*/
+    }
 
     public static void showArray() {
         array [0] = 0;
@@ -19,7 +21,7 @@ public class Ejercicio1 {
             System.out.println(array[i]);
         }
     }
-   public static int [] maxArray(int[] array){
+   public static int maxArray(int[] array){
        int max = array[0];
         for (int i = 0; i < array.length; i++) {
           if (array[i] > max) {
@@ -27,9 +29,9 @@ public class Ejercicio1 {
           }
 
         }
-       return new int[]{max} ;
+       return max ;
    }
-   public static int [] minArray(int[] array){
+   public static int minArray(int[] array){
        int min = array[0];
        for (int i = 0; i < array.length; i++) {
            if (array[i] < min) {
@@ -37,18 +39,18 @@ public class Ejercicio1 {
            }
 
        }
-       return new int[]{min} ;
+       return min;
    }
-   public static int[] mediaArray(int[] array){
+   public static int mediaArray(int[] array){
        int sumatory = array[0];
        for (int i = 0; i < array.length; i++) {
-           sumatory += array[i];
+           sumatory += array[i] /2;
            }
-       return new int[]{sumatory};
+       return sumatory;
    }
 
-    public static boolean[] existeArray(int[] array) {
-
+    public static void existeArray(int[] array) {
+        System.out.println("Introduce un valor:");
         int number = scanner.nextInt();
         array [0] = 0;
         for (int i = 0; i < array.length; i++) {
@@ -57,9 +59,8 @@ public class Ejercicio1 {
                 System.out.println("El número existe.");
             }else {
                 System.out.println("El número no existe.");
+                break;
             }
         }
-        return new boolean[0];
-        //?
     }
 }
