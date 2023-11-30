336acad5c3f63b58c53667d84663708b79ac714c
juamarval5 <valcarceljuandavid@gmail.com>
2023-11-30 12:13:05 +0100

    Mosca

--- Tema3/tema3/AtrapaMosca.java
+++ Tema3/tema3/AtrapaMosca.java
@@ -1,8 +1,16 @@
 package tema3;
 
+import java.util.Scanner;
+
 public class AtrapaMosca {
-    //Array x15
-    //Pon un número
+    static int[] arrayFly = new int[15];
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Averigua donde está la mosca, introduciendo un número: ");
+        int number = scanner.nextInt();
+        if ()
+    }
     //Si aciertas, ganas
     //Si la mosca esta en +1 o -1 se mueve
 
