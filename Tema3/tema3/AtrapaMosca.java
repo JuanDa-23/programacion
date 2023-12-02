dafdfb85c6a0a67e08dd103e3f261ee964a011ff
Condicionales <valcarceljuandavid@gmail.com>
2023-12-02 11:38:55 +0100

    Array

--- Tema3/tema3/AtrapaMosca.java
+++ Tema3/tema3/AtrapaMosca.java
@@ -1,17 +1,31 @@
 package tema3;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class AtrapaMosca {
-    static int[] arrayFly = new int[15];
-    static Scanner scanner = new Scanner(System.in);
+    static int[] arrayFly = new int[16];
+    static Random random = new Random();
+    static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Averigua donde está la mosca, introduciendo un número: ");
-        int number = scanner.nextInt();
-     //   if ()
-    }
-    //Si aciertas, ganas
-    //Si la mosca esta en +1 o -1 se mueve
 
+        arrayFly = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+        System.out.println("Busca la mosca! Pon un número del 0 al 15: ");
+        boolean cazada = false;
+        while (!cazada) {
+            int posicion = random.nextInt(15);
+            int[] arrayMosca = arrayFly.clone();
+            int numero = in.nextInt();
+            arrayMosca[posicion] = 1;
+            if (arrayMosca[numero] == 1) {
+                System.out.println("Has cazado a la mosca!");
+                cazada = true;
+            } else {
+                System.out.println("La mosca cambio de ubicación");
+            }
+        }
+
+    }
 }
