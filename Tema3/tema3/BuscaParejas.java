d7cc6124714b3beaf4f1912f6dd9f2a5aaec7bb5
Condicionales <valcarceljuandavid@gmail.com>
2023-12-05 22:38:51 +0100

    Array

--- Tema3/tema3/BuscaParejas.java
+++ Tema3/tema3/BuscaParejas.java
@@ -6,70 +6,53 @@ import java.util.Scanner;
 
 public class BuscaParejas {
     public static void main(String[] args) {
-        String[] animales = {"león", "mandril", "suricata", "facóquero", "tigre", "elefante", "jirafa", "mono", "cocodrilo", "cebra"};
-        String[] parejas = new String[animales.length * 2];
-        for (int i = 0; i < animales.length; i++) {
-            parejas[i] = animales[i];
-            parejas[i + animales.length] = animales[i];
-        }
-        Collections.shuffle(Arrays.asList(parejas));
-        boolean[] visibles = new boolean[parejas.length];
+        String[] animales = {"león", "mandril", "suricata", "león", "mandril", "suricata", "tigre", "elefante", "rinoceronte", "jirafa",
+                "tigre", "elefante", "rinoceronte", "jirafa", "cocodrilo", "hipopótamo", "cebra", "cocodrilo", "hipopótamo", "cebra"};
+        Collections.shuffle(Arrays.asList(animales));
+        boolean[] visibles = new boolean[animales.length];
+        Arrays.fill(visibles, false);
         Scanner scanner = new Scanner(System.in);
-        int parejasRestantes = animales.length;
-        while (parejasRestantes > 0) {
-        mostrarTablero(parejas, visibles);
-
-
-            System.out.println("Ingresa la primera posición: ");
+        System.out.println(Arrays.toString(animales));
+        while (!todosVisibles(visibles)) {
+            mostrarTablero(animales, visibles);
+            System.out.print("Ingrese la primera posición: ");
             int posicion1 = scanner.nextInt();
-
-            System.out.println("Ingresa la segunda posición: ");
+            System.out.print("Ingrese la segunda posición: ");
             int posicion2 = scanner.nextInt();
-
-
-            if (posicionValida(posicion1, parejas.length) && posicionValida(posicion2, parejas.length) && !visibles[posicion1] && !visibles[posicion2]) {
-
-                if (parejas[posicion1].equals(parejas[posicion2])) {
-
-                    System.out.println("¡Encontraste una pareja!");
-                    visibles[posicion1] = true;
-                    visibles[posicion2] = true;
-                    parejasRestantes--;
-                } else {
-
-                    System.out.println("¡No son iguales! Inténtalo de nuevo.");
-                }
-            } else {
-
-                System.out.println("Posiciones inválidas. Inténtalo de nuevo.");
+            if (posicion1 < 1 || posicion1 > animales.length || posicion2 < 1 || posicion2 > animales.length || posicion1 == posicion2) {
+                System.out.println("Entradas no válidas.");
+                continue;
             }
-
-            limpiarPantalla();
+            if (animales[posicion1 - 1].equals(animales[posicion2 - 1])) {
+                visibles[posicion1 - 1] = true;
+                visibles[posicion2 - 1] = true;
+                System.out.println("¡Encontraste una pareja!");
+            } else {
+                System.out.println("¡Las posiciones no coinciden! Inténtalo de nuevo.");
+            }
+            for (int i = 0; i < 50; ++i) System.out.println();
         }
-
-
         System.out.println("¡Felicidades! Has encontrado todas las parejas.");
-        scanner.close();
     }
 
-    public static void mostrarTablero(String[] parejas, boolean[] visibles) {
-        for (int i = 0; i < parejas.length; i++) {
+    public static void mostrarTablero(String[] animales, boolean[] visibles) {
+        System.out.println("Tablero:");
+        for (int i = 0; i < animales.length; i++) {
             if (visibles[i]) {
-                System.out.print(parejas[i] + " ");
+                System.out.print(animales[i] + "\t");
             } else {
-                System.out.print("?? ");
+                System.out.print("?" + "\t");
             }
         }
         System.out.println();
     }
 
-    public static void limpiarPantalla() {
-        for (int i = 0; i < 50; i++) {
-            System.out.println();
+    public static boolean todosVisibles(boolean[] visibles) {
+        for (boolean visible : visibles) {
+            if (!visible) {
+                return false;
+            }
         }
-    }
-
-    public static boolean posicionValida(int posicion, int longitudArray) {
-        return posicion >= 0 && posicion < longitudArray;
+        return true;
     }
 }
