687188ef4cb90985daf93fb9cfccd8b8efd24edd
Condicionales <valcarceljuandavid@gmail.com>
2023-12-11 19:52:20 +0100

    Matrix

--- Tema3/tema3/MatrixEj1.java
+++ Tema3/tema3/MatrixEj1.java
@@ -7,27 +7,9 @@ public class MatrixEj1 {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-
-    }
-    public static void creatMatrix(Matrix1[][]) {
-        System.out.println("Introduce los valores de la matriz: ");
-
-        int[][] Matrix1 = new int[3][3];
-        for (int i = 0; i < Matrix1.length; i++) {
-            for (int j = 0; j < Matrix1[0].length; j++) {
-                System.out.print("Valor " + i + " " + j + ":");
-                Matrix1[i][j] = scanner.nextInt();
-            }
-        }
-
-        System.out.println(Arrays.deepToString(Matrix1));
-    }
-
-
+     System.out.println(creatMatrix());
 }
-
-
-    /*public static int[][] creatMatrix() {
+      public static int[][] creatMatrix() {
         System.out.println("Introduce los valores de la matriz: ");
 
         int[][] Matrix1 = new int[3][3];
@@ -40,5 +22,20 @@ public class MatrixEj1 {
 
         System.out.println(Arrays.deepToString(Matrix1));
         return Matrix1;
-    }*/
+      }
 
+}
+
+
+
+
+   /* public static void creatMatrix(int[][] Matrix1) {
+        for (int i = 0; i < Matrix1.length; i++) {
+            for (int j = 0; j < Matrix1[i].length; j++) {
+                System.out.print(Matrix1[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println(Arrays.deepToString(Matrix1));
+    }*/
