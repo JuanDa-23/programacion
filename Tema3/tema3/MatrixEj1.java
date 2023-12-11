ce8aae98f9d6d0f313a6f231d4e6c0c61a9687ee
juamarval5 <valcarceljuandavid@gmail.com>
2023-12-11 10:03:49 +0100

    ...

--- Tema3/tema3/MatrixEj1.java
+++ Tema3/tema3/MatrixEj1.java
@@ -7,10 +7,27 @@ public class MatrixEj1 {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        creatMatrix();
+
+    }
+    public static void creatMatrix(Matrix1[][]) {
+        System.out.println("Introduce los valores de la matriz: ");
+
+        int[][] Matrix1 = new int[3][3];
+        for (int i = 0; i < Matrix1.length; i++) {
+            for (int j = 0; j < Matrix1[0].length; j++) {
+                System.out.print("Valor " + i + " " + j + ":");
+                Matrix1[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println(Arrays.deepToString(Matrix1));
     }
 
-    public static int[][] creatMatrix() {
+
+}
+
+
+    /*public static int[][] creatMatrix() {
         System.out.println("Introduce los valores de la matriz: ");
 
         int[][] Matrix1 = new int[3][3];
@@ -23,8 +40,5 @@ public class MatrixEj1 {
 
         System.out.println(Arrays.deepToString(Matrix1));
         return Matrix1;
-    }
-
-
-}
+    }*/
 
