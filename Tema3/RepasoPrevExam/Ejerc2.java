f9aa28696da94219d4d52d6c7e4ec259bcc445e9
Condicionales <valcarceljuandavid@gmail.com>
2023-12-14 00:13:18 +0100

    Matrix

--- Tema3/RepasoPrevExam/Ejerc2.java
+++ Tema3/RepasoPrevExam/Ejerc2.java
@@ -3,9 +3,34 @@ package RepasoPrevExam;
 import java.util.Scanner;
 
 public class Ejerc2 {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-
+        /*if (calcularRectangulo() >= 0){
+            System.out.println("El resultado es " + calcularRectangulo());
+        }else {
+            System.out.println("Porfavor, introduce valores apropiados.");
+        }*/
+        calcularRectangulo();
     }
+    public static int calcularRectangulo(){
+        System.out.println("Introduce la base de un rectangulo: ");
+        int base = scanner.nextInt();
+        System.out.println("Introduce la altura del rectangulo");
+        int altura = scanner.nextInt();
+        solicitarNumero("Porfavor ingresa valores apropiados.",1,1000);
+
+        int result = (base * 2) + (altura * 2);
+        return result;
+    }
+    public static int solicitarNumero(String mensaje, int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        do {
+            System.out.print(mensaje);
+            numero = scanner.nextInt();
+            if (numero < min || numero > max) System.out.println("Los valores no son posibles. Vuelve a introducir el numero correcto");
+        } while (numero < min || numero > max);
+        return numero;
+    }
+
 }
