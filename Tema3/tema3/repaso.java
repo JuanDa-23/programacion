3402e80ac05648aa85e6c7b1da03d465a48ccb5e
Condicionales <valcarceljuandavid@gmail.com>
2024-01-11 20:01:14 +0100

    Strings y repaso

--- Tema3/tema3/repaso.java
+++ Tema3/tema3/repaso.java
@@ -9,24 +9,6 @@ public class repaso {
 
         System.out.println("Introduce la hora: ");
         int hora = in.nextInt();
-        System.out.println("Introduce los minutos: ");
-        int minuto = in.nextInt();
-        System.out.println("Introduce segundo: ");
-        int segundo = in.nextInt();
-
-        if (hora <= 0 || minuto <= 0 || segundo <= 0) {
-            System.out.println("ERROR");
-        } else {
-            System.out.println("La hora es: " + hora + ":" + minuto + ":" + (segundo + 1));
-        }
-
-        if (segundo > 60) {
-            System.out.println("La hora es: " + hora + ":" + (minuto + 1) + ":" + (segundo - 60));
-        } else if (minuto > 60) {
-            System.out.println("La hora es: " + (hora + 1) + ":" + (minuto - 60) + ":" + segundo);
-        } else if (hora > 23) {
-            System.out.println("La hora es: " + (hora - 23) + ":" + minuto + ":" + (segundo + 1));
-        }
 
     }
 }
