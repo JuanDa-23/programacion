0575f4749c7def72c1ec20ac19d23ac995ff4141
Condicionales <valcarceljuandavid@gmail.com>
2024-01-11 19:45:20 +0100

    Strings y repaso

--- Tema3/tema3/repaso.java
+++ Tema3/tema3/repaso.java
@@ -3,23 +3,30 @@ package tema3;
 import java.util.Scanner;
 
 public class repaso {
-        static Scanner in = new Scanner(System.in);
-        public static void main(String[] args) {
+    static Scanner in = new Scanner(System.in);
 
-            System.out.println("Introduce la hora: ");
-            int hora = in.nextInt();
-            System.out.println("Introduce los minutos: ");
-            int minuto = in.nextInt();
-            System.out.println("Introduce segundo: ");
-            int segundo = in.nextInt();
+    public static void main(String[] args) {
 
-            if(hora > 23 || hora < 0 || minuto > 60 || minuto < 0 || segundo > 60 || segundo < 0) {
-                System.out.println("ERROR");
-            } else {
-                System.out.println("La hora es: " + hora + ":" + minuto + ":" + (segundo + 1));
-            }
+        System.out.println("Introduce la hora: ");
+        int hora = in.nextInt();
+        System.out.println("Introduce los minutos: ");
+        int minuto = in.nextInt();
+        System.out.println("Introduce segundo: ");
+        int segundo = in.nextInt();
 
-
+        if (hora <= 0 || minuto <= 0 || segundo <= 0) {
+            System.out.println("ERROR");
+        } else {
+            System.out.println("La hora es: " + hora + ":" + minuto + ":" + (segundo + 1));
         }
 
+        if (segundo > 60) {
+            System.out.println("La hora es: " + hora + ":" + (minuto + 1) + ":" + (segundo - 60));
+        } else if (minuto > 60) {
+            System.out.println("La hora es: " + (hora + 1) + ":" + (minuto - 60) + ":" + segundo);
+        } else if (hora > 23) {
+            System.out.println("La hora es: " + (hora - 23) + ":" + minuto + ":" + (segundo + 1));
+        }
+
+    }
 }
