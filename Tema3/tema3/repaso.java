2182e8fe2209a972e87d97c92b7b24a3e24fe2cd
Condicionales <valcarceljuandavid@gmail.com>
2024-01-11 18:29:41 +0100

    Strings y repaso

--- Tema3/tema3/repaso.java
+++ Tema3/tema3/repaso.java
@@ -3,7 +3,6 @@ package tema3;
 import java.util.Scanner;
 
 public class repaso {
-    public class Repaso1 {
         static Scanner in = new Scanner(System.in);
         public static void main(String[] args) {
 
@@ -16,12 +15,11 @@ public class repaso {
 
             if(hora > 23 || hora < 0 || minuto > 60 || minuto < 0 || segundo > 60 || segundo < 0) {
                 System.out.println("ERROR");
-            } else if (segundo ){
             } else {
                 System.out.println("La hora es: " + hora + ":" + minuto + ":" + (segundo + 1));
             }
 
 
         }
-    }
+
 }
