084db483950f849ea30c2dfe6d0973c027dc63eb
Condicionales <valcarceljuandavid@gmail.com>
2024-01-17 20:20:52 +0100

    Strings y repaso

--- Tema3/tema3/repaso.java
+++ Tema3/tema3/repaso.java
@@ -8,12 +8,31 @@ import java.util.Scanner;
 public class repaso {
     static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Introduce tu edad: ");
-        int number = in.nextInt();
-
+       showMenu();
+        int money = in.nextInt();
+        char option = in.next().toUpperCase().charAt(0);
+        switch (option){
+            case 'E':
+                dol2eu(money);
+                break;
+            case 'D':
+                eu2dol(money);
+                break;
+        }
     }
-    public static
-
-
+    public static void showMenu(){
+        System.out.println(" Intrroduce el dinero: \n" +
+                "Introduce la E o la D \n" +
+                "E = dolar to euro \n" +
+                "D = euro to dollar.");
+    }
+    public static double eu2dol(double money){
+        money = money * 1.92;
+        return money;
+    }
+    public static double dol2eu(double money){
+        money = money * 0.90;
+        return money;
+    }
 
 }
