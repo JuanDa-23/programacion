071dd81720c03153f82b34a24d2b8418a5dd3473
Condicionales <valcarceljuandavid@gmail.com>
2024-01-17 15:42:09 +0100

    Strings y repaso

--- Tema3/tema3/repaso.java
+++ Tema3/tema3/repaso.java
@@ -8,6 +8,22 @@ import java.util.Scanner;
 public class repaso {
     static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
+        System.out.println("Introduce tu edad: ");
+        int number = in.nextInt();
+        if (!isAdult(number)){
+            System.out.println("Eres mayor de edad");
+        }else {
+            System.out.println("Eres menor de edad");
+        }
 
+        }
+
+    public static boolean isAdult(int number){
+        boolean result = true;
+        if (number >= 18){
+            result = false;
+        }
+        return result;
     }
+
 }
