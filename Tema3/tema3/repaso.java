5797039ed9b30d29cf42f430258630f16a801d0c
juamarval5 <valcarceljuandavid@gmail.com>
2024-01-18 08:56:38 +0100

    String

--- Tema3/tema3/repaso.java
+++ Tema3/tema3/repaso.java
@@ -6,15 +6,9 @@ public class repaso {
     static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Introduce una fecha para ser validada: ");
-        int day = in.nextInt();
-        int mounth = in.nextInt();
-        int year = in.nextInt();
+
 
     }
-
-   /* public static boolean trueDate(int day,int mounth, int year) {
-
-    }*/
-
 }
+
+
