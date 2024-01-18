b8099f9a20c616179f1a21751b5eef5dbb9d124b
juamarval5 <valcarceljuandavid@gmail.com>
2024-01-18 12:24:23 +0100

    String

--- Tema3/tema3/repaso.java
+++ Tema3/tema3/repaso.java
@@ -4,21 +4,45 @@ import java.util.Scanner;
 
 public class repaso {
     static Scanner in = new Scanner(System.in);
-
+    static int[] array = new int[6];
     public static void main(String[] args) {
         //System.out.println("Introduce la letra para elegir el ejercicio: ");
         //char letter = in.next().toLowerCase().charAt(0);
-            creatArray();
+
+        creatArray();
+        System.out.println(maxArray(array));
+        System.out.println(minArray(array));
+
+
+
     }
 
     public static void creatArray() {
         System.out.println("Introduce los valores del array: ");
-        int[] array = new int[6];
         for (int i = 0; i < array.length; i++) {
            array[i] = in.nextInt();
         }
 
     }
+    public static int maxArray(int array[]){
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+    public static int minArray(int array[]){
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
 }
 
 
