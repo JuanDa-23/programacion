7a8e89eae9ff858d83ffe0f536a70415d80a3b52
juamarval5 <valcarceljuandavid@gmail.com>
2024-01-18 09:21:08 +0100

    String

--- Tema3/tema3/repaso.java
+++ Tema3/tema3/repaso.java
@@ -6,6 +6,19 @@ public class repaso {
     static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("Introduce el tamaño: ");
+        int number = in.nextInt();
+        System.out.println("Introduce la letra: ");
+        char caracter = in.next().charAt(0);
+        madeTringle(number, caracter);
+    }
+
+    public static void madeTringle(int number, char caracter) {
+        for (int i = 0; i < number; i++) {
+            System.out.println(caracter + " ".repeat(number) + "\n");
+
+
+        }
 
 
     }
