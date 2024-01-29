6c6c699aa46c31d123222eac2b6640dc4106fa46
Condicionales <valcarceljuandavid@gmail.com>
2024-01-29 17:16:11 +0100

    Strings y repaso

--- Tema3/tema3/repaso.java
+++ Tema3/tema3/repaso.java
@@ -6,10 +6,21 @@ public class repaso {
     static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        //System.out.println("Introduce la letra para elegir el ejercicio: ");
+        System.out.println("Hay una mosca en alguna casilla, seleciona una casilla: ");
         //char letter = in.next().toLowerCase().charAt(0);
+        int casilla = in.nextInt();
+        if (){
+
+        }else if (){
+
+        }
 
     }
+
+    public static int buscaMoscas(int[] array) {
+
+    }
+
 }
 
 
