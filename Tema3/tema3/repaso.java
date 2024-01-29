5a59aedbb58b35d68567d0dc8883ed0f3fe9b311
Condicionales <valcarceljuandavid@gmail.com>
2024-01-29 17:11:57 +0100

    Strings y repaso

--- Tema3/tema3/repaso.java
+++ Tema3/tema3/repaso.java
@@ -12,10 +12,6 @@ public class repaso {
     }
 }
 
+
 //¿Porque hay veces que indice 0 = valor 0?
-// 4 = 3 + 1 es correcto
-//.toString => hacer el Array bonito
-//if (array2.length == array.length) => igualar indices
-/*funcion int[] = me devuelve un array
-funcion int (pero uso arrays) = devuelve enteros
- */
+
