dc3f583bb29befaca8e7c60e97341aaa55efd873
juamarval5 <valcarceljuandavid@gmail.com>
2024-01-30 10:51:12 +0100

    String

--- Tema3/tema3/repaso.java
+++ Tema3/tema3/repaso.java
@@ -7,14 +7,40 @@ public class repaso {
     static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
+        int[] arrayMosca = new int[15];
+        for (int i = 0; i < arrayMosca.length; i++) {
+            arrayMosca[i] = 0;
+        }
         Random random = new Random();
-        System.out.println("Hay una mosca en alguna casilla, seleciona una casilla: ");
-        //char letter = in.next().toLowerCase().charAt(0);
+        int n = random.nextInt(0, 15);
+        arrayMosca[n] = 8;
 
+       /* IF arrayMosca[USUARIO] == 8
+                HAS GANADO
+        ELSE
+            ARRAYMOSCA[USUARIO+1]
+        ARRAYMOSCA[USUARIO-1]
+                RANDOM.NEXTINT(0,15)
+                        ARRAYMOSCA[RANDOM] = 8;*/
+        /*Random random = new Random();
+        System.out.println("Hay una mosca en alguna casilla, seleciona una casilla: ");
+        boolean cazada = false;
+        int[] arrayMosca = new int[15];
+        while (!cazada){
+
+        }
+        if (){
+
+        }
+
+        int mosca = in.nextInt();
+        int casilla = random.nextInt();
+
+     */
     }
 
 }
 
-
+//char letter = in.next().toLowerCase().charAt(0);
 //¿Porque hay veces que indice 0 = valor 0?
 
